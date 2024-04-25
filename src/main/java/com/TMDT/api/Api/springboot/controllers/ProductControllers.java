f25e2df157aad01a871c0af33a4b0e9772558141
giabaoitwork <139120902+giabaoitwork@users.noreply.github.com>
2024-04-25 19:25:53 +0700

    Update ProductControllers.java

--- src/main/java/com/TMDT/api/Api/springboot/controllers/ProductControllers.java
+++ src/main/java/com/TMDT/api/Api/springboot/controllers/ProductControllers.java
@@ -5,10 +5,7 @@ import com.TMDT.api.Api.springboot.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -42,5 +39,34 @@ public class ProductControllers {
                 );
     }
 
+    @PostMapping("/insert")
+    ResponseEntity<ResponseObject> insertProduct(@RequestBody Product newProduct) {
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "success", productRepository.save(newProduct))
+        );
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<ResponseObject> updateProduct(@PathVariable int id, @RequestBody Product newProduct) {
+        Product productUpdate = productRepository.findById(id)
+                .map(product -> { // neu tim thay product thi chinh sua thong tin
+                    product.setName(newProduct.getName());
+                    product.setDescription(newProduct.getDescription());
+                    product.setImage(newProduct.getImage());
+                    product.setImages(newProduct.getImages());
+                    product.setPrice(newProduct.getPrice());
+                    product.setDiscount(newProduct.getDiscount());
+                    product.setSold(newProduct.getSold());
+                    product.setQuantity(newProduct.getQuantity());
+                    product.setStatus(newProduct.getStatus());
+                    return productRepository.save(product);
+                }).orElseGet(() -> {  // neu khong tim thay thi them product moi
+                    newProduct.setId(id);
+                    return productRepository.save(newProduct);
+                });
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "success", productUpdate)
+        );
+    }
 
 }
